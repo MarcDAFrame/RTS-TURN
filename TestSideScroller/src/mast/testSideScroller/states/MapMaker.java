526efe7c07b93eed7283f17c0055076c9911be2e
Marc Frame <mframe45@gmail.com>
2015-12-15 18:28:27 -0500

    started works on MapMaker.java. and changed the buttons around to make it look better

--- TestSideScroller/src/mast/testSideScroller/states/MapMaker.java
+++ TestSideScroller/src/mast/testSideScroller/states/MapMaker.java
@@ -3,6 +3,7 @@ package mast.testSideScroller.states;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -10,7 +11,10 @@ import org.newdawn.slick.state.StateBasedGame;
 import mast.testSideScroller.tools.States;
 
 public class MapMaker extends BasicGameState{
-
+	
+	//selectedBlock 0 = nothing, 1 = dirt, 2 = stone, 3 = metal, 4 = specialBlock, 5 = air1, 6 = air2, 7 = air3, 8 = air4
+	static int selectedBlock = 0;
+	
 	@Override
 	public void init(GameContainer gc, StateBasedGame game) throws SlickException {
 		// TODO Auto-generated method stub
@@ -26,6 +30,23 @@ public class MapMaker extends BasicGameState{
 	@Override
 	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
 		// TODO Auto-generated method stub
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_1)){
+			selectedBlock = 1;
+			//dirt
+		}
+		if(input.isKeyDown(Input.KEY_2)){
+			selectedBlock = 2;
+			//stone
+		}
+		if(input.isKeyDown(Input.KEY_3)){
+			selectedBlock = 3;
+			//metal
+		}
+		if(input.isKeyDown(Input.KEY_4)){
+			selectedBlock = 4;
+			//specialBlock
+		}
 		
 	}
 
