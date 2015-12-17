da12d4bb6a57e745852ac1d0459296ed7ccd9688
Marc Frame <mframe45@gmail.com>
2015-12-16 22:10:18 -0500

    worked on the Map.java and started on the mapParser() this creates a new folder where we will save the maps (saves folder) there is a readMe

--- TestSideScroller/src/mast/testSideScroller/tools/Map.java
+++ TestSideScroller/src/mast/testSideScroller/tools/Map.java
@@ -4,14 +4,25 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 public class Map {
-	
+	private static int mapWidth = 0, mapHeight = 0;
 	private static Image[][] tileSheetArray;
+	private static String[][] mapData;
 	
 	public static void mapInit(){
 		tileSheetArray = Resources.spriteSheetCutter(Resources.returnImage("tileSheet"), 256, 256);
 	}
 	
 	public static void mapParse() {
+		String tempMapLineData = null;
+		for(int i = 0; i < mapWidth; i++){
+			for(int j = 0; j < mapWidth; j++){
+				mapData[i][j] = tempMapLineData.split(",");
+			}
+			
+			tempMapLineData = null;
+		}
+	}
+	public static void mapWrite(){
 		
 	}
 	
